452f8fcce19d8accad9584c1fcf271f849237757
nakflix <123237114+nakflix@users.noreply.github.com>
2023-12-06 09:18:21 -0800

    Update env.ts

--- env.ts
+++ env.ts
@@ -4,7 +4,7 @@ import { cleanEnv, str } from "envalid";
 await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
-  BOT_TOKEN: str(),
-  OWNERS: str(),
-  MONGO_URL: str(),
+  BOT_TOKEN: str(6406498841:AAHuZyB5xlBG586N6rJcpIHc9K4QgxFDgQg),
+  OWNERS: str(1458235021),
+  MONGO_URL: str(mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority),
 });
